67b5d2fe15d6e540c28192cc0990a211a6df3a14
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-15 23:23:59 +0200

    Create ORDER BY.sql

---
+++ MySQL/ORDER BY.sql
@@ -0,0 +1,27 @@
+/* 
+MySQL statements with ORDER BY 
+*/
+
+--------------- 1 ------------------
+SELECT * FROM Automobiles
+ORDER BY Engine;
+
+--------------- 2 ------------------
+SELECT * FROM Automobiles
+ORDER BY Engine DESC;
+
+--------------- 3 ------------------
+SELECT * FROM Automobiles
+ORDER BY Engine, CountryOrigin, Manufacturer;
+
+--------------- 4 ------------------
+SELECT * FROM Automobiles
+ORDER BY Engine ASC, CountryOrigin ASC, Manufacturer DESC;
+
+--------------- 5 -----------------
+
+SELECT Engine, CountryOrigin, Manufacturer
+FROM Automobiles
+WHERE Manufacturer = 'Fiat'
+ORDER BY Manufacturer DESC;
+
